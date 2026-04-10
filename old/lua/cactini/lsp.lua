6c2f7e0a24d2f17eaf97e1541af58b44afed386f
cactini <dear.sag@gmail.com>
2026-04-10 11:49:39 +0200

    ignore old floder
    I just created it to cross reference :>

---
+++ old/lua/cactini/lsp.lua
@@ -0,0 +1,4 @@
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("clangd")
+vim.lsp.enable("cmake")
+vim.lsp.enable("pylsp")
---
+++ old/lua/cactini/remap.lua
@@ -0,0 +1,117 @@
+--
+-- DEFAULTS
+--
+
+-- Leader space
+vim.g.mapleader = " "
+
+-- File manager
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- jj escape
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- Move with JK
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Append with a space without moving the cursor J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in the middle with C-d and C-u
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Delete and replace to void, and copy to clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Change project through tmux
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- Create vertical split
+vim.keymap.set("n", "<leader>vs", "<C-w>v")
+
+-- Split navigation with Ctrl + hjkl
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Quicklist navigation or smth idk TBD
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace all
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Turn file into executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
+
+--
+-- TELESCOPE
+--
+
+-- Requirements
+local builtin = require('telescope.builtin')
+
+-- Find files
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+
+-- Find git files
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
+
+-- Grep in files
+vim.keymap.set('n', '<leader>ps', function() 
+    builtin.grep_string({ search = vim.fn.input("Grep > ")});
+end)
+
+-- Find document symbols
+vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = 'Document symbols' })
+
+--
+-- HARPOON
+--
+
+-- Requirements
+local harpoon = require("harpoon")
+harpoon:setup()
+
+-- Add
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+
+-- View files
+vim.keymap.set("n", "<leader>A", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+-- Goto file n
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
+vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
+vim.keymap.set("n", "<leader>0", function() harpoon:list():select(10) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>n", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>N", function() harpoon:list():next() end)
+
+-- 
+-- TINY-CODE-ACTIONS
+--
+
+-- Add code actions with <leader>ca
+vim.keymap.set({ "n", "x" }, "<leader>ca", function()
+	require("tiny-code-action").code_action()
+end, { noremap = true, silent = true })
